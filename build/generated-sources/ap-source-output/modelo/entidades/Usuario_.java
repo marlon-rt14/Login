85ab208c1dfda1dd31f58fcb34c0cd87f6aa5693
marlon-rt14 <mjavierrt14@mail.com>
2020-12-24 19:29:57 -0500

    Fix: Plataforma JDK 1.8 para una correcta conexion con la base de datos.

---
+++ build/generated-sources/ap-source-output/modelo/entidades/Usuario_.java
@@ -0,0 +1,15 @@
+package modelo.entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-24T19:14:42")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> clave;
+    public static volatile SingularAttribute<Usuario, String> usuario;
+    public static volatile SingularAttribute<Usuario, Integer> id;
+
+}
